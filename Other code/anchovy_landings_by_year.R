bb5a49a6d5c75d5916c2a4313e23c7a864442e3a
Felipe Quezada <fequezad@ucsc.edu>
2022-05-13 13:45:20 -0700

    Update anchovy_landings_by_year.R

--- Other code/anchovy_landings_by_year.R
+++ Other code/anchovy_landings_by_year.R
@@ -24,12 +24,16 @@ Tickets<-rbind(Tickets1, Tickets2, Tickets3, Tickets4)
 rm(Tickets1, Tickets2, Tickets3, Tickets4)
 
 
+
+
 ## Subset the data to get remove columns not relevant to this analysis. This will speed things up.
-Tickets <- dplyr::select(Tickets, c(PACFIN_SPECIES_CODE, LANDED_WEIGHT_MTONS, LANDING_YEAR, LANDING_MONTH))
+Tickets <- dplyr::select(Tickets, c(PACFIN_SPECIES_CODE, LANDED_WEIGHT_MTONS, LANDING_YEAR, LANDING_MONTH, 
+                                    AGENCY_CODE, VESSEL_OWNER_ADDRESS_STATE))
 options(scipen=999)  
-Tickets_anchovy <- Tickets %>% dplyr::filter(PACFIN_SPECIES_CODE == 'NANC') %>% group_by(LANDING_YEAR, LANDING_MONTH) %>%
+Tickets_anchovy <- Tickets %>% dplyr::filter(PACFIN_SPECIES_CODE == 'NANC') %>% 
+  group_by(LANDING_YEAR, LANDING_MONTH, AGENCY_CODE) %>%
     summarize(NANC_Landings = sum(LANDED_WEIGHT_MTONS))
 
-write.csv(Tickets_anchovy,"C:\\Data\\Anchovy_landings.csv", row.names = FALSE)
+write.csv(Tickets_anchovy,"C:\\Data\\Anchovy_landings_by_state.csv", row.names = FALSE)
 
   
