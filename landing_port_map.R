b4ca98b2bf8ff583375474c59cce0267ece3f3fd
Felipe Quezada <fequezad@ucsc.edu>
2023-02-14 22:15:16 -0800

    Update landing_port_map.R

--- landing_port_map.R
+++ landing_port_map.R
@@ -7,3 +7,24 @@ library(ggspatial)
 library(rnaturalearth)
 library(tidygeocoder)
 library(maps)
+library(ggplot2)
+
+# Delete old data #
+rm(list=ls())
+gc()
+
+
+# Get map data #
+world_map_data <- ne_countries(scale = "medium", returnclass = "sf")
+state_map_data <- map('state', fill = TRUE, plot = FALSE) %>% st_as_sf()
+
+# Get port coordinates #
+port_data <- read.csv(here::here("Data", "Ports", "port_areas.csv"))
+
+
+# Plot U.S. West Coast map #
+ggplot() +
+  geom_sf(data = world_map_data) +
+  geom_sf(data = state_map_data) +
+  geom_point(data = port_data, aes(x = lon, y = lat), color = 'red') +
+  coord_sf(xlim = c(-126, -117), ylim = c(32,49))
