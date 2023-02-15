56d67db6d1df6c5922aec32c2c1dc41fc58cf6ba
Felipe Quezada <fequezad@ucsc.edu>
2023-02-15 12:00:36 -0800

    Update landing_port_map.R

--- landing_port_map.R
+++ landing_port_map.R
@@ -13,7 +13,6 @@ library(ggplot2)
 rm(list=ls())
 gc()
 
-
 # Get map data #
 world_map_data <- ne_countries(scale = "medium", returnclass = "sf")
 state_map_data <- map('state', fill = TRUE, plot = FALSE) %>% st_as_sf()
@@ -22,9 +21,40 @@ state_map_data <- map('state', fill = TRUE, plot = FALSE) %>% st_as_sf()
 port_data <- read.csv(here::here("Data", "Ports", "port_areas.csv"))
 
 
-# Plot U.S. West Coast map #
-ggplot() +
-  geom_sf(data = world_map_data) +
-  geom_sf(data = state_map_data) +
-  geom_point(data = port_data, aes(x = lon, y = lat), color = 'red') +
-  coord_sf(xlim = c(-126, -117), ylim = c(32,49))
+#---------------------
+# Create theme for map
+#---------------------
+
+mytheme <- theme(text = element_text(family = 'Avenir')
+                 ,panel.grid.major = element_line(color = '#cccccc' 
+                                                    ,linetype = 'dashed'
+                                                  ,size = .3
+                 )
+                 ,panel.background = element_rect(fill = 'aliceblue')
+                 ,plot.title = element_text(size = 32)
+                 ,plot.subtitle = element_text(size = 14)
+                 ,axis.title = element_blank()
+                 ,axis.text = element_text(size = 10)
+)
+
+land_color <- c('antiquewhite1')
+
+#--------------------------
+# Plot U.S. West Coast map 
+#--------------------------
+base_plot <- ggplot() +
+  geom_sf(data = world_map_data, fill = land_color, size = .4) +
+  geom_sf(data = state_map_data, fill = NA, size = .4) +
+  geom_point(data = port_data, aes(x = lon, y = lat), size = 4, color = 'red', alpha = .15) +
+  geom_point(data = port_data, aes(x = lon, y = lat), size  = 4, shape = 1,  color = 'red') +
+  coord_sf(xlim = c(-126.5, -117.5), ylim = c(34,48.5)) +
+  mytheme
+
+# SHOW MAP
+print(base_plot)
+
+
+
+
+
+
