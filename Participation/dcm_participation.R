6a89e0295a8b9d50081ec6fde50e4b8cd5022b83
Felipe Quezada <fequezad@ucsc.edu>
2023-03-03 18:31:53 -0800

    Update dcm_participation.R

--- Participation/dcm_participation.R
+++ Participation/dcm_participation.R
@@ -224,90 +224,84 @@ rdo <- samps %>% dplyr::select(fished, fished_haul,dummy_miss, mean_rev, mean_re
 # rdo <- rdo %>% group_by(fished_haul) %>% mutate(alt_tow = 1:length(fished_haul)) %>% as.data.frame
 
 
-#-----------------------------------------------------------------------------
-## Fit mlogit models returning the coefficients, the models, and the data going into the model
-
-#Fit the model for everything at once
-library(data.table)
-library(mlogit)
-
-
-## Drop choice cards that received no choice
-# rdo2 <- rdo %>% 
-#   group_by(fished_haul) %>% 
-#   mutate(full = sum(fished)) %>% 
-#   filter(full!=0) %>% 
-#   select(-c(full))
-
-
-# Check hauls in same day (only 30)
-fished_haul_select <- rdo %>%
-  group_by(fished_VESSEL_NUM, set_date) %>%
-  mutate(ncount = n()) %>% dplyr::filter(ncount == 5) %>% ungroup() %>%
-  select('fished_haul') %>% unique()
-
-rdo <- data.table::setDT(rdo)[fished_haul %chin% fished_haul_select$fished_haul]
-
-
-rdo2 <- as.data.frame(rdo[order(rdo$fished_VESSEL_NUM, rdo$fished_haul, -rdo$fished),]) %>%
-  group_by(fished_VESSEL_NUM) %>%
-  dplyr::mutate(fished_VESSEL_ID = cur_group_id()) %>%
-  ungroup() %>% dplyr::select(-c('fished_VESSEL_NUM')) %>%
-  group_by(set_date) %>%
-  dplyr::mutate(time = cur_group_id()) %>%
-  ungroup() %>% dplyr::select(-c('set_date'))
-
-
-write.csv(rdo2,"C:\\GitHub\\EconAnalysis\\Data\\sampled_mixed_logit_data.csv", row.names = FALSE)
-
-
-str(rdo2)
-## Maybe create new fished_haul number within VESSEL_NUM?
-
-the_tows <- mlogit.data(rdo2, shape = 'long', choice = 'fished', alt.var = 'selection', 
-                        id.var = "fished_VESSEL_ID", chid.var = "fished_haul")
-
-# drop.index
-# mf <- mFormula(fished ~ dummy_miss | 1 | mean_rev_adj)
-# res <- mlogit(mf, the_tows)
+# #-----------------------------------------------------------------------------
+# ## Fit mlogit models returning the coefficients, the models, and the data going into the model
+# 
+# #Fit the model for everything at once
+# library(data.table)
+# library(mlogit)
+# 
+# 
+# ## Drop choice cards that received no choice
+# # rdo2 <- rdo %>% 
+# #   group_by(fished_haul) %>% 
+# #   mutate(full = sum(fished)) %>% 
+# #   filter(full!=0) %>% 
+# #   select(-c(full))
+# 
+# 
+# # Check hauls in same day (only 30)
+# fished_haul_select <- rdo %>%
+#   group_by(fished_VESSEL_NUM, set_date) %>%
+#   mutate(ncount = n()) %>% dplyr::filter(ncount == 5) %>% ungroup() %>%
+#   select('fished_haul') %>% unique()
+# 
+# rdo <- data.table::setDT(rdo)[fished_haul %chin% fished_haul_select$fished_haul]
+# 
+# 
+# rdo2 <- as.data.frame(rdo[order(rdo$fished_VESSEL_NUM, rdo$fished_haul, -rdo$fished),]) %>%
+#   group_by(fished_VESSEL_NUM) %>%
+#   dplyr::mutate(fished_VESSEL_ID = cur_group_id()) %>%
+#   ungroup() %>% dplyr::select(-c('fished_VESSEL_NUM')) %>%
+#   group_by(set_date) %>%
+#   dplyr::mutate(time = cur_group_id()) %>%
+#   ungroup() %>% dplyr::select(-c('set_date'))
+# 
+# 
+# write.csv(rdo2,"C:\\GitHub\\EconAnalysis\\Data\\sampled_mixed_logit_data.csv", row.names = FALSE)
+# 
+# 
+# str(rdo2)
+# 
+# 
+# 
+# ## Maybe create new fished_haul number within VESSEL_NUM?
+# 
+# the_tows <- mlogit.data(rdo2, shape = 'long', choice = 'fished', alt.var = 'selection', 
+#                         id.var = "fished_VESSEL_ID", chid.var = "fished_haul")
+# 
+# # drop.index
+# # mf <- mFormula(fished ~ dummy_miss | 1 | mean_rev_adj)
+# # res <- mlogit(mf, the_tows)
+# # summary(res)
+# 
+# res <- mlogit(fished ~ dummy_miss + mean_rev_adj,
+#               the_tows, reflevel = 'No-Participation', panel = TRUE, 
+#               rpar = c(mean_rev_adj = "n"),
+#               correlation = FALSE, R = 100, halton = NA)
 # summary(res)
-
-res <- mlogit(fished ~ dummy_miss + mean_rev_adj,
-              the_tows, reflevel = 'No-Participation', panel = TRUE, 
-              rpar = c(mean_rev_adj = "n"),
-              correlation = FALSE, R = 100, halton = NA)
-summary(res)
-
-
-###
-# Error in if (abs(x - oldx) < ftol) { : 
-#     missing value where TRUE/FALSE needed --- Export to Stata?
-
-#--------------------------------------------------------------------
-#Generate and format the predictions
-fits <- fitted(res, outcome = FALSE)
-mfits <- reshape2::melt(fits) %>% 
-  dplyr::rename(fished_haul = Var1) %>%
-  dplyr::rename(selection_pred = Var2)
-
-## -- Correct prediction using max probability value -- ##
-pred_tows <- mfits %>% group_by(fished_haul) %>% filter(value == max(value)) %>% as.data.frame
-pred_tows <- merge(rdo, pred_tows, by = "fished_haul") %>% filter(fished == TRUE) %>% 
-  mutate(correct = ifelse(selection_pred == selection, 1, 0))
-correct_prediction <- sum(pred_tows$correct) / nrow(pred_tows) #score 1
-correct_prediction
-
-
-pred_tows2 <- pred_tows %>% dplyr::filter(selection != "No-Participation")
-correct_prediction <- sum(pred_tows2$correct) / nrow(pred_tows2) #score 1
-correct_prediction
-
-
-
-
-
-
-
-
-
+# 
+# 
+# ###
+# # Error in if (abs(x - oldx) < ftol) { : 
+# #     missing value where TRUE/FALSE needed --- Export to Stata?
+# 
+# #--------------------------------------------------------------------
+# #Generate and format the predictions
+# fits <- fitted(res, outcome = FALSE)
+# mfits <- reshape2::melt(fits) %>% 
+#   dplyr::rename(fished_haul = Var1) %>%
+#   dplyr::rename(selection_pred = Var2)
+# 
+# ## -- Correct prediction using max probability value -- ##
+# pred_tows <- mfits %>% group_by(fished_haul) %>% filter(value == max(value)) %>% as.data.frame
+# pred_tows <- merge(rdo, pred_tows, by = "fished_haul") %>% filter(fished == TRUE) %>% 
+#   mutate(correct = ifelse(selection_pred == selection, 1, 0))
+# correct_prediction <- sum(pred_tows$correct) / nrow(pred_tows) #score 1
+# correct_prediction
+# 
+# 
+# pred_tows2 <- pred_tows %>% dplyr::filter(selection != "No-Participation")
+# correct_prediction <- sum(pred_tows2$correct) / nrow(pred_tows2) #score 1
+# correct_prediction
 
