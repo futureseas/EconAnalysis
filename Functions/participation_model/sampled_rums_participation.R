5d112f99ac90195e86e98eff69b5a093c33c7138
Felipe Quezada <fequezad@ucsc.edu>
2023-04-24 16:33:47 -0700

    update participation model

--- Functions/participation_model/sampled_rums_participation.R
+++ Functions/participation_model/sampled_rums_participation.R
@@ -55,11 +55,13 @@ sampled_rums <- function(data_in, cluster = 4,
   
   
   #-----------------------------------------------------------------------------
-  ## Estimate models for landings
+  ## Estimate models for landings (conditional vessel have decide to participate)
   datPanel <- dat %>% 
     dplyr::filter(selection != "No-Participation") %>% 
     dplyr::filter(set_year >= min_year, set_year <= max_year)
 
+  
+  #############
   ### Pacific sardine landing model ###
   datPanel_PSDN <- datPanel %>% filter(Species_Dominant == "PSDN") %>%
     dplyr::mutate(Closure = ifelse(set_date >= "2008-05-29" & set_date < "2008-07-01", 1, 0)) %>%
@@ -76,36 +78,66 @@ sampled_rums <- function(data_in, cluster = 4,
     dplyr::mutate(Closure = ifelse(set_date >= "2011-09-21" & set_date < "2012-01-01", 1, 0)) %>%
     dplyr::mutate(Closure = ifelse(set_date >= "2012-08-23" & set_date < "2012-09-01", 1, 0)) %>%
     dplyr::mutate(Closure = ifelse(set_date >= "2013-08-22" & set_date < "2013-09-01", 1, 0)) %>%
-    dplyr::mutate(Closure = ifelse(set_date >= "2015-04-28", 1, 0))
+    dplyr::mutate(Closure = ifelse(set_date >= "2015-04-28", 1, 0)) %>% filter(Closure == 0)
 
-  qPSDN <- lm(Landings_mtons ~ lag_PSDN_SDM_90 + factor(VESSEL_NUM) + factor(set_month) + 
-                factor(set_year) + factor(PORT_AREA_CODE) + factor(Closure), data = datPanel_PSDN)
-  
-  # summary(qPSDN)
+  qPSDN <- lm(Landings_mtons ~ lag_PSDN_SDM_60 + factor(VESSEL_NUM)  + factor(set_year) + factor(set_month), data = datPanel_PSDN)
+  summary(qPSDN)
   
   # qPSDN <- estimatr::lm_robust(Landings_mtons ~ lag_PSDN_SDM_90 +
   #                                factor(VESSEL_NUM) + factor(set_month) + factor(set_year) + factor(Closure),
   #                              data = datPanel_PSDN, clusters = group_all, se_type = "stata")
   
-  
+  ##############
   ### Market squid landing model ### (Maybe use lagged prices? ADD WEEKEND!)
-  datPanel_MSQD<- datPanel %>% filter(Species_Dominant == "MSQD") %>%
+  datPanel_MSQD <- datPanel %>% filter(Species_Dominant == "MSQD") %>%
     dplyr::mutate(Closure = ifelse(set_date >= "2010-12-17" & set_date < "2011-03-31", 1, 0)) %>%
     dplyr::mutate(Closure = ifelse(set_date >= "2011-11-18" & set_date < "2012-03-31", 1, 0)) %>%
-    dplyr::mutate(Closure = ifelse(set_date >= "2012-11-21" & set_date < "2013-03-31", 1, 0))
-  
-  datPanel_MSQD <- datPanel_MSQD %>% dplyr::mutate(weekend = ifelse(chron::is.weekend(set_date), 1, 0))
-  
-  
-  qMSQD <- lm(Landings_mtons ~ lag_MSQD_SDM_90 + factor(set_month) +
-                factor(VESSEL_NUM) + factor(set_year) + factor(Closure) + 
-                factor(weekend) + factor(PORT_AREA_CODE), data = datPanel_MSQD)
-  
+    dplyr::mutate(Closure = ifelse(set_date >= "2012-11-21" & set_date < "2013-03-31", 1, 0)) %>% 
+    dplyr::mutate(weekend = ifelse(chron::is.weekend(set_date), 1, 0)) %>%
+    filter(weekend == 0) %>%  filter(Closure == 0)
+  qMSQD <- lm(Landings_mtons ~ lag_MSQD_SDM_90 + factor(VESSEL_NUM)  + factor(set_year) + factor(set_month), data = datPanel_MSQD)
   summary(qMSQD)
   
+  
+  ##############
+  ### Pacific herring landing model 
+  datPanel_PHRG <- datPanel %>% filter(Species_Dominant == "PHRG") 
+  qPHRG <- lm(Landings_mtons ~ lag_PHRG_SDM_220 + factor(VESSEL_NUM) + factor(set_year) + factor(set_month), data = datPanel_PHRG)
+  summary(qPHRG)
+  
+  
+  ##############
+  ### Northern anchovy
+  datPanel_NANC <- datPanel %>% filter(Species_Dominant == "NANC") 
+  qNANC<- lm(Landings_mtons ~ lag_NANC_SDM_30 + factor(VESSEL_NUM) + factor(set_year) + factor(set_month), data = datPanel_NANC)
+  summary(qNANC)
+  
+  
+  # ##############
+  # ### Jack Mackerel
+  # datPanel_JMCK <- datPanel %>% filter(Species_Dominant == "JMCK") 
+  # qJMCK <- lm(Landings_mtons ~ lag_JMCK_SDM_90 + factor(VESSEL_NUM) + factor(set_year) + factor(set_month) + factor(PORT_AREA_CODE), data = datPanel_JMCK)
+  # summary(qJMCK)
+  # 
+  # 
+  # ##############
+  # ### Chub mackerel
+  # datPanel_CMCK <- datPanel %>% filter(Species_Dominant == "CMCK") 
+  # qCMCK <- lm(Landings_mtons ~ lag_CMCK_SDM_90 + factor(VESSEL_NUM) + factor(set_year) + factor(set_month), data = datPanel_CMCK)
+  # summary(qCMCK)
+  
+  
+  #########################################
   ## Create table for paper (all species)
   
-  # modelsummary::modelsummary(qPSDN, output = "landings_models.docx")
+    models <- list(
+    "Pacific sardine"     = qPSDN,
+    "Market squid" = qMSQD,
+    "Northern anchovy"     = qNANC,
+    "Pacific herring" = qPHRG)
+  modelsummary::modelsummary(models, output = "landings_models.docx")
+  
+  
   
   
   #--------------------------------
