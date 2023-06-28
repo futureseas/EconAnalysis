548c7b8a44cfda95cc571cae89633749507cb657
Felipe Quezada <fequezad@ucsc.edu>
2023-06-27 20:26:56 -0700

    update sdm

--- Participation/SDMs/NANC_sdm.R
+++ Participation/SDMs/NANC_sdm.R
@@ -15,25 +15,25 @@ library(geosphere)
 
 #------------------------------------------------------
 # Get SDM coordinates
-dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/anchovy/anch_6_2010_GAM.nc"))
+dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/Historical CPS GAMs BRTs ERDDAP ROMS Domain/6_2010_SDMs.nc"))
 
   set_long <- ncdf4::ncvar_get(dat, "lon")
   set_lat <- ncdf4::ncvar_get(dat, "lat")
   nanc.date.sdm <- ncdf4::ncvar_get(dat, "time")
-  nanc.sdm <- ncdf4::ncvar_get(dat, "predGAM")
+  nanc.sdm <- ncdf4::ncvar_get(dat, "anchGAM")
   ncdf4::nc_close(dat)
 
   # Reshape the 3D array so we can map it, change the time field to be date
   dimnames(nanc.sdm) <- list(set_long = set_long, set_lat = set_lat, nanc.date.sdm = nanc.date.sdm)
   sdmMelt <- reshape2::melt(nanc.sdm, value.name = "nanc.sdm")
-  coord <- sdmMelt %>% dplyr::select(c(set_lat, set_long)) %>% unique()
-  saveRDS(coord, "Participation/SDM_code/port_dist_nanc/coord_sdm_nanc.rds")
+  coord_sdm <- sdmMelt %>% dplyr::select(c(set_lat, set_long)) %>% unique()
+  saveRDS(coord_sdm, "Participation/SDMs/port_dist_nanc/coord_sdm_nanc.rds")
 
 
 #------------------------------------------------------
 # Get distance by coordinate to port j and save it
 
-coord <- readRDS("Participation/SDM_code/port_dist_nanc/coord_sdm_nanc.rds")
+coord <- readRDS("Participation/SDMs/port_dist_nanc/coord_sdm_nanc.rds")
 port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>% drop_na()
 
 for (j in 1:nrow(port_area_coord)) {
@@ -44,7 +44,7 @@ for (j in 1:nrow(port_area_coord)) {
     mutate(dist = dist / 1000) %>% 
     dplyr::filter(dist <= 220)
   
-  saveRDS(distPorts, paste0("Participation/SDM_code/port_dist_nanc/portDist_", paste0(as.character(j),".rds")))
+  saveRDS(distPorts, paste0("Participation/SDMs/port_dist_nanc/portDist_", paste0(as.character(j),".rds")))
 }
 
 #----------------------------
@@ -56,24 +56,25 @@ sdm.nanc <- tibble(LANDING_YEAR = integer(),
                    PORT_AREA_CODE = character(),
                    NANC_SDM_20 = numeric(),
                    NANC_SDM_30 = numeric(),
-                   NANC_SDM_90 = numeric(),
-                   NANC_SDM_220 = numeric())
+                   NANC_SDM_60 = numeric())
 
 port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>% drop_na()
 
-for (y in 2000:2018) {
+for (y in 2000:2020) {
   for (m in 1:12) {
     for (j in 1:nrow(port_area_coord)) {
       
       # Open the monthly file that contain SDM by location
-      dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/anchovy/anch_",
-                                   paste0(as.character(m),
-                                          paste0("_", paste0(as.character(y),"_GAM.nc")))))
+      dat <- ncdf4::nc_open(
+        paste0("G:/My Drive/Data/SDM/Historical CPS GAMs BRTs ERDDAP ROMS Domain/",
+               paste0(as.character(m),
+                      paste0("_",
+                             paste0(as.character(y),"_SDMs.nc")))))
       
       set_long <- ncdf4::ncvar_get(dat, "lon")
       set_lat <- ncdf4::ncvar_get(dat, "lat")
       nanc.date.sdm <- ncdf4::ncvar_get(dat, "time")
-      nanc.sdm <- ncdf4::ncvar_get(dat, "predGAM")
+      nanc.sdm <- ncdf4::ncvar_get(dat, "anchGAM")
       ncdf4::nc_close(dat)			
       
       # Reshape the 3D array so we can map it, change the time field to be date
@@ -82,8 +83,9 @@ for (y in 2000:2018) {
       sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$nanc.date.sdm)	
       
       # Merge to distance from port area to a set of coordinates
-      distPorts <- readRDS(paste0("Participation/SDM_code/port_dist_nanc/portDist_",
-                                  paste0(as.character(j), ".rds")))
+      distPorts <- readRDS(
+        paste0("Participation/SDMs/port_dist_nanc/portDist_",
+               paste0(as.character(j), ".rds")))
       sdmMelt <- merge(sdmMelt, distPorts, by = c('set_lat', 'set_long'), all.x = TRUE, all.y = FALSE) %>% 
         mutate(LANDING_DAY = day(set_date))
       
@@ -91,12 +93,10 @@ for (y in 2000:2018) {
       for (z in 1:max(sdmMelt$LANDING_DAY)) {
         dat_prob_20  <- sdmMelt %>% dplyr::filter(dist <= 20) %>% dplyr::filter(LANDING_DAY == z)
         dat_prob_30  <- sdmMelt %>% dplyr::filter(dist <= 30) %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_90  <- sdmMelt %>% dplyr::filter(dist <= 90) %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_220 <- sdmMelt %>% dplyr::filter(dist <= 220) %>% dplyr::filter(LANDING_DAY == z)
+        dat_prob_60  <- sdmMelt %>% dplyr::filter(dist <= 60) %>% dplyr::filter(LANDING_DAY == z)
         SDM_mean_20  <- mean(dat_prob_20$nanc.sdm, na.rm = TRUE)
         SDM_mean_30  <- mean(dat_prob_30$nanc.sdm, na.rm = TRUE)
-        SDM_mean_90  <- mean(dat_prob_90$nanc.sdm, na.rm = TRUE)
-        SDM_mean_220 <- mean(dat_prob_220$nanc.sdm, na.rm = TRUE)
+        SDM_mean_60  <- mean(dat_prob_60$nanc.sdm, na.rm = TRUE)
         sdm.nanc <- sdm.nanc %>%
           add_row(LANDING_YEAR = y, 
                   LANDING_MONTH = m, 
@@ -104,64 +104,12 @@ for (y in 2000:2018) {
                   PORT_AREA_CODE = as.character(port_area_coord[j, 1]), 
                   NANC_SDM_20  = SDM_mean_20, 
                   NANC_SDM_30  = SDM_mean_30, 
-                  NANC_SDM_90  = SDM_mean_90, 
-                  NANC_SDM_220 = SDM_mean_220)
-        rm(dat_prob_20, dat_prob_30, dat_prob_90, dat_prob_220, 
-           SDM_mean_20, SDM_mean_30, SDM_mean_90, SDM_mean_220)
+                  NANC_SDM_60  = SDM_mean_60)
+        rm(dat_prob_20, dat_prob_30, dat_prob_60, 
+           SDM_mean_20, SDM_mean_30, SDM_mean_60)
       }
       print(paste("Year:", y, "; month:", m, "--", "Port area:",j))
-      saveRDS(sdm.nanc, file = "Participation/SDM_code/sdm_nanc.rds")
+      saveRDS(sdm.nanc, file = "Participation/SDMs/sdm_nanc_update.rds")
     }
   }
 }
-
-for (m in 1:8) {
-    for (j in 1:nrow(port_area_coord)) {
-      
-      # Open the monthly file that contain SDM by location
-      dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/anchovy/anch_",
-                                   paste0(as.character(m), "_2019_GAM.nc")))
-      
-      set_long <- ncdf4::ncvar_get(dat, "lon")
-      set_lat <- ncdf4::ncvar_get(dat, "lat")
-      nanc.date.sdm <- ncdf4::ncvar_get(dat, "time")
-      nanc.sdm <- ncdf4::ncvar_get(dat, "predGAM")
-      ncdf4::nc_close(dat)			
-      
-      # Reshape the 3D array so we can map it, change the time field to be date
-      dimnames(nanc.sdm) <- list(set_long = set_long, set_lat = set_lat, nanc.date.sdm = nanc.date.sdm)
-      sdmMelt <- reshape2::melt(nanc.sdm, value.name = "nanc.sdm")
-      sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$nanc.date.sdm)	
-      
-      # Merge to distance from port area to a set of coordinates
-      distPorts <- readRDS(paste0("Participation/SDM_code/port_dist_nanc/portDist_",
-                                  paste0(as.character(j), ".rds")))
-      sdmMelt <- merge(sdmMelt, distPorts, by = c('set_lat', 'set_long'), all.x = TRUE, all.y = FALSE) %>% 
-        mutate(LANDING_DAY = day(set_date))
-      
-      # Calculate daily SDM level within port radius
-      for (z in 1:max(sdmMelt$LANDING_DAY)) {
-        dat_prob_20  <- sdmMelt %>% dplyr::filter(dist <= 20)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_30  <- sdmMelt %>% dplyr::filter(dist <= 30)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_90  <- sdmMelt %>% dplyr::filter(dist <= 90)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_220 <- sdmMelt %>% dplyr::filter(dist <= 220) %>% dplyr::filter(LANDING_DAY == z)
-        SDM_mean_20  <- mean(dat_prob_20$nanc.sdm, na.rm = TRUE)
-        SDM_mean_30  <- mean(dat_prob_30$nanc.sdm, na.rm = TRUE)
-        SDM_mean_90  <- mean(dat_prob_90$nanc.sdm, na.rm = TRUE)
-        SDM_mean_220 <- mean(dat_prob_220$nanc.sdm, na.rm = TRUE)
-        sdm.nanc <- sdm.nanc %>%
-          add_row(LANDING_YEAR = 2019, 
-                  LANDING_MONTH = m, 
-                  LANDING_DAY = z, 
-                  PORT_AREA_CODE = as.character(port_area_coord[j, 1]), 
-                  NANC_SDM_20  = SDM_mean_20, 
-                  NANC_SDM_30  = SDM_mean_30, 
-                  NANC_SDM_90  = SDM_mean_90, 
-                  NANC_SDM_220 = SDM_mean_220)
-        rm(dat_prob_20, dat_prob_30, dat_prob_90, dat_prob_220, 
-           SDM_mean_20, SDM_mean_30, SDM_mean_90, SDM_mean_220)
-      }
-      print(paste("Year: 2019; month:", m, "--", "Port area:",j))
-      saveRDS(sdm.nanc, file = "Participation/SDM_code/sdm_nanc.rds")
-    }
-  }
--- Participation/SDMs/PSDN_sdm.R
+++ Participation/SDMs/PSDN_sdm.R
@@ -13,10 +13,40 @@ library(lubridate)
 library(tidyr)
 library(geosphere)
 
-# Get port area coordinates
-port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>%
-  drop_na()
 
+#------------------------------------------------------
+# Get SDM coordinates
+dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/Historical CPS GAMs BRTs ERDDAP ROMS Domain/6_2010_SDMs.nc"))
+
+set_long <- ncdf4::ncvar_get(dat, "lon")
+set_lat <- ncdf4::ncvar_get(dat, "lat")
+psdn.date.sdm <- ncdf4::ncvar_get(dat, "time")
+psdn.sdm <- ncdf4::ncvar_get(dat, "sardGAM")
+ncdf4::nc_close(dat)
+
+# Reshape the 3D array so we can map it, change the time field to be date
+dimnames(psdn.sdm) <- list(set_long = set_long, set_lat = set_lat, psdn.date.sdm = psdn.date.sdm)
+sdmMelt <- reshape2::melt(psdn.sdm, value.name = "psdn.sdm")
+coord <- sdmMelt %>% dplyr::select(c(set_lat, set_long)) %>% unique()
+saveRDS(coord, "Participation/SDMs/port_dist_psdn/coord_sdm_psdn.rds")
+
+
+#------------------------------------------------------
+# Get distance by coordinate to port j and save it
+
+coord <- readRDS("Participation/SDMs/port_dist_psdn/coord_sdm_psdn.rds")
+port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>% drop_na()
+
+for (j in 1:nrow(port_area_coord)) {
+  distPorts <- coord %>%
+    mutate(dist = by(., 1:nrow(.), function(row) {
+      distHaversine(c(row$set_long, row$set_lat), c(port_area_coord[j,]$lon, port_area_coord[j,]$lat))
+    })) %>%
+    mutate(dist = dist / 1000) %>% 
+    dplyr::filter(dist <= 220)
+  
+  saveRDS(distPorts, paste0("Participation/SDMs/port_dist_psdn/portDist_", paste0(as.character(j),".rds")))
+}
 
 #----------------
 # Pacific sardine
@@ -26,23 +56,24 @@ sdm.psdn <- tibble(LANDING_YEAR = integer(),
                    LANDING_DAY = integer(),
                    PORT_AREA_CODE = character(),
                    PSDN_SDM_30 = numeric(),
-                   PSDN_SDM_60 = numeric(),
-                   PSDN_SDM_90 = numeric(),
-                   PSDN_SDM_220 = numeric())
+                   PSDN_SDM_60 = numeric())
 
-for (y in 2015:2018) {
+for (y in 2000:2020) {
   for (m in 1:12) {
     for (j in 1:nrow(port_area_coord)) {
       
       # Open the monthly file that contain SDM by location
-      dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/sardine/sard_",
-                                   paste0(as.character(m),
-                                          paste0("_", paste0(as.character(y),"_GAM.nc")))))
+      dat <- 
+        ncdf4::nc_open(
+          paste0("G:/My Drive/Data/SDM/Historical CPS GAMs BRTs ERDDAP ROMS Domain/",
+                 paste0(as.character(m),
+                        paste0("_",
+                               paste0(as.character(y),"_SDMs.nc")))))
       
       set_long <- ncdf4::ncvar_get(dat, "lon")
       set_lat <- ncdf4::ncvar_get(dat, "lat")
       psdn.date.sdm <- ncdf4::ncvar_get(dat, "time")
-      psdn.sdm <- ncdf4::ncvar_get(dat, "predGAM")
+      psdn.sdm <- ncdf4::ncvar_get(dat, "sardGAM")
       ncdf4::nc_close(dat)			
       
       # Reshape the 3D array so we can map it, change the time field to be date
@@ -50,114 +81,30 @@ for (y in 2015:2018) {
       sdmMelt <- reshape2::melt(psdn.sdm, value.name = "psdn.sdm")
       sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$psdn.date.sdm)	
       
-      sdmMelt <- sdmMelt %>%
-        group_by(set_lat, set_long, set_date) %>%
-        summarize(exp_prob = mean(psdn.sdm, na.rm = T))	%>%
-        ungroup(.)  
-      sdmMelt <- sdmMelt %>% group_by(set_lat, set_long) %>%
-        dplyr::mutate(ID_location = cur_group_id()) %>%
-        ungroup(.) 
-      
-      # Calculate distance from port area to a set of coordinates (using ID location)
-      dist_to_ID <- sdmMelt %>% select(c(set_long, set_lat, ID_location)) %>% unique() %>%
-        mutate(dist = by(., 1:nrow(.), function(row) {
-          distHaversine(c(row$set_long, row$set_lat), c(port_area_coord[j,]$lon, port_area_coord[j,]$lat))
-        })) %>%
-        mutate(dist = dist / 1000) %>% select(-c(set_long, set_lat))
-      
-      sdmMelt <- merge(sdmMelt, dist_to_ID, by = c('ID_location'), all.x = TRUE, all.y = FALSE) %>% 
-        mutate(LANDING_DAY = day(set_date)) 
+      # Merge to distance from port area to a set of coordinates
+      distPorts <- readRDS(
+        paste0("Participation/SDMs/port_dist_psdn/portDist_",
+               paste0(as.character(j), ".rds")))
+      sdmMelt <- merge(sdmMelt, distPorts, by = c('set_lat', 'set_long'), all.x = TRUE, all.y = FALSE) %>% 
+        mutate(LANDING_DAY = day(set_date))
       
       # Calculate daily SDM level within port radius
       for (z in 1:max(sdmMelt$LANDING_DAY)) {
         dat_prob_30  <- sdmMelt %>% dplyr::filter(dist <= 30)  %>% dplyr::filter(LANDING_DAY == z)
         dat_prob_60  <- sdmMelt %>% dplyr::filter(dist <= 60)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_90  <- sdmMelt %>% dplyr::filter(dist <= 90)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_220 <- sdmMelt %>% dplyr::filter(dist <= 220) %>% dplyr::filter(LANDING_DAY == z)
         SDM_mean_30  <- mean(dat_prob_30$exp_prob, na.rm = TRUE)
         SDM_mean_60  <- mean(dat_prob_60$exp_prob, na.rm = TRUE)
-        SDM_mean_90  <- mean(dat_prob_90$exp_prob, na.rm = TRUE)
-        SDM_mean_220 <- mean(dat_prob_220$exp_prob, na.rm = TRUE)
         sdm.psdn <- sdm.psdn %>%
           add_row(LANDING_YEAR = y, 
                   LANDING_MONTH = m, 
                   LANDING_DAY = z, 
                   PORT_AREA_CODE = as.character(port_area_coord[j, 1]), 
                   PSDN_SDM_30  = SDM_mean_30, 
-                  PSDN_SDM_60  = SDM_mean_60, 
-                  PSDN_SDM_90  = SDM_mean_90, 
-                  PSDN_SDM_220 = SDM_mean_220)
-        rm(dat_prob_30, dat_prob_60, dat_prob_90, dat_prob_220, 
-           SDM_mean_30, SDM_mean_60, SDM_mean_90, SDM_mean_220)
+                  PSDN_SDM_60  = SDM_mean_60)
+        rm(dat_prob_30, dat_prob_60, SDM_mean_30, SDM_mean_60)
       }
       print(paste("Year:", y, "; month:", m, "--", "Port area:",j))
-      readr::write_csv(sdm.psdn, file = "Participation/SDM_code/sdm.psdn_v2.csv")
+      saveRDS(sdm.psdn, file = "Participation/SDMs/sdm_psdn.rds")
     }
   }
 }
-
-### Add 2019 data until august
-  for (m in 1:8) {
-    for (j in 1:nrow(port_area_coord)) {
-      
-      # Open the monthly file that contain SDM by location
-      dat <- ncdf4::nc_open(paste0("G:/My Drive/Data/SDM/sardine/sard_",
-                                   paste0(as.character(m),
-                                          paste0("_2019_GAM.nc"))))
-      
-      set_long <- ncdf4::ncvar_get(dat, "lon")
-      set_lat <- ncdf4::ncvar_get(dat, "lat")
-      psdn.date.sdm <- ncdf4::ncvar_get(dat, "time")
-      psdn.sdm <- ncdf4::ncvar_get(dat, "predGAM")
-      ncdf4::nc_close(dat)			
-      
-      # Reshape the 3D array so we can map it, change the time field to be date
-      dimnames(psdn.sdm) <- list(set_long = set_long, set_lat = set_lat, psdn.date.sdm = psdn.date.sdm)
-      sdmMelt <- reshape2::melt(psdn.sdm, value.name = "psdn.sdm")
-      sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$psdn.date.sdm)	
-      
-      sdmMelt <- sdmMelt %>%
-        group_by(set_lat, set_long, set_date) %>%
-        summarize(exp_prob = mean(psdn.sdm, na.rm = T))	%>%
-        ungroup(.)  
-      sdmMelt <- sdmMelt %>% group_by(set_lat, set_long) %>%
-        dplyr::mutate(ID_location = cur_group_id()) %>%
-        ungroup(.) 
-      
-      # Calculate distance from port area to a set of coordinates (using ID location)
-      dist_to_ID <- sdmMelt %>% select(c(set_long, set_lat, ID_location)) %>% unique() %>%
-        mutate(dist = by(., 1:nrow(.), function(row) {
-          distHaversine(c(row$set_long, row$set_lat), c(port_area_coord[j,]$lon, port_area_coord[j,]$lat))
-        })) %>%
-        mutate(dist = dist / 1000) %>% select(-c(set_long, set_lat))
-      
-      sdmMelt <- merge(sdmMelt, dist_to_ID, by = c('ID_location'), all.x = TRUE, all.y = FALSE) %>% 
-        mutate(LANDING_DAY = day(set_date))
-      
-      
-      # Calculate daily SDM level within port radius
-      for (z in 1:max(sdmMelt$LANDING_DAY)) {
-        dat_prob_30  <- sdmMelt %>% dplyr::filter(dist <= 30)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_60  <- sdmMelt %>% dplyr::filter(dist <= 60)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_90  <- sdmMelt %>% dplyr::filter(dist <= 90)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_220 <- sdmMelt %>% dplyr::filter(dist <= 220) %>% dplyr::filter(LANDING_DAY == z)
-        SDM_mean_30  <- mean(dat_prob_30$exp_prob, na.rm = TRUE)
-        SDM_mean_60  <- mean(dat_prob_60$exp_prob, na.rm = TRUE)
-        SDM_mean_90  <- mean(dat_prob_90$exp_prob, na.rm = TRUE)
-        SDM_mean_220 <- mean(dat_prob_220$exp_prob, na.rm = TRUE)
-        sdm.psdn <- sdm.psdn %>%
-          add_row(LANDING_YEAR = 2019, 
-                  LANDING_MONTH = m, 
-                  LANDING_DAY = z, 
-                  PORT_AREA_CODE = as.character(port_area_coord[j, 1]), 
-                  PSDN_SDM_30  = SDM_mean_30, 
-                  PSDN_SDM_60  = SDM_mean_60, 
-                  PSDN_SDM_90  = SDM_mean_90, 
-                  PSDN_SDM_220 = SDM_mean_220)
-        rm(dat_prob_30, dat_prob_60, dat_prob_90, dat_prob_220, 
-           SDM_mean_30, SDM_mean_60, SDM_mean_90, SDM_mean_220)
-      }
-      print(paste("Year:", 2019, "; month:", m, "--", "Port area:",j))
-      readr::write_csv(sdm.psdn, file = "Participation/SDM_code/sdm.psdn_v2.csv")
-    }
-  }
