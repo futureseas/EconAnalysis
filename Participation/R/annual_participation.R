2ca2861866967c7e1b7d4f95f361fbe0d38a72a7
Felipe Quezada <fequezad@ucsc.edu>
2023-12-19 17:11:47 -0800

    Update annual_participation.R

--- Participation/R/annual_participation.R
+++ Participation/R/annual_participation.R
@@ -243,11 +243,11 @@ gc()
 ### Estimate model ###
 ######################
 
-annual.part3 <- readRDS("C:/Data/PacFIN data/annual_part.RDS") %>% dplyr::filter(group_all == 4)
+annual.part4 <- readRDS("C:/Data/PacFIN data/annual_part.RDS") %>% dplyr::filter(group_all == 4)
 
 logit <- brm(active_year ~  years_active + mean_HHI + mean_COG + mean_LI + 
                mean_PRICE + mean_SDM + (1 | VESSEL_NUM), 
-            data = annual.part3, seed = 123, family = bernoulli(link = "logit"), warmup = 500, 
+            data = annual.part4, seed = 123, family = bernoulli(link = "logit"), warmup = 500, 
             iter = 2000, chain = 1, cores = 4)
             summary(logit)
             plot(conditional_effects(logit), points = TRUE)
