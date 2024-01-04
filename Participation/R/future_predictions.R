7a49c6b5d92368c1b50bc660772d8dcaff3266f2
Felipe Quezada <fequezad@ucsc.edu>
2024-01-03 16:15:00 -0800

    Create future_predictions.R

---
+++ Participation/R/future_predictions.R
@@ -0,0 +1,20 @@
+####################################
+## Future predictions using SDMs ###
+####################################
+
+
+
+## Create database
+
+
+
+
+## Load SDMs future predictions
+
+
+
+## Load annual participation model
+
+annual_model <- readRDS("logit_all_2.RDS")
+
+
