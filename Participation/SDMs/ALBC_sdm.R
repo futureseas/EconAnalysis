9d7561fde1d032fe5ad0d45c5b907b1189a9f983
Felipe Quezada <fequezad@ucsc.edu>
2024-01-19 16:49:25 -0800

    update

--- Participation/SDMs/ALBC_sdm.R
+++ Participation/SDMs/ALBC_sdm.R
@@ -28,13 +28,13 @@ ncdf4::nc_close(dat)
 dimnames(albc.sdm) <- list(set_long = set_long, set_lat = set_lat, albc.date.sdm = albc.date.sdm)
 sdmMelt <- reshape2::melt(albc.sdm, value.name = "albc.sdm")
 coord <- sdmMelt %>% dplyr::select(c(set_lat, set_long)) %>% unique()
-saveRDS(coord, "Participation/SDMs/port_dist_psdn/coord_sdm_albc.rds")
+saveRDS(coord, "Participation/SDMs/port_dist_albc/coord_sdm_albc.rds")
 
 
 #------------------------------------------------------
 # Get distance by coordinate to port j and save it
 
-coord <- readRDS("Participation/SDMs/port_dist_psdn/coord_sdm_albc.rds")
+coord <- readRDS("Participation/SDMs/port_dist_albc/coord_sdm_albc.rds")
 port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>% drop_na()
 
 for (j in 1:nrow(port_area_coord)) {
@@ -43,21 +43,22 @@ for (j in 1:nrow(port_area_coord)) {
       distHaversine(c(row$set_long, row$set_lat), c(port_area_coord[j,]$lon, port_area_coord[j,]$lat))
     })) %>%
     mutate(dist = dist / 1000) %>% 
-    dplyr::filter(dist <= 220)
+    dplyr::filter(dist <= 180)
   
   saveRDS(distPorts, paste0("Participation/SDMs/port_dist_albc/portDist_", paste0(as.character(j),".rds")))
 }
 
 #----------------
-# Pacific sardine
+# Albacore
 
-sdm.psdn <- tibble(LANDING_YEAR = integer(),
+sdm.albc <- tibble(LANDING_YEAR = integer(),
                    LANDING_MONTH = integer(),
                    LANDING_DAY = integer(),
                    PORT_AREA_CODE = character(),
-                   ALBC_SDM_60 = numeric(),
-                   ALBC_SDM_110 = numeric(),
-                   ALBC_SDM_220 = numeric())
+                   albc_SDM_90 = numeric(),
+                   albc_SDM_180 = numeric())
+
+### https://wdfw.wa.gov/fishing/commercial/tuna 50 to 100 nautical miles
 
 port_area_coord <- read.csv("C:\\GitHub\\EconAnalysis\\Data\\Ports\\port_areas.csv") %>% drop_na()
 
@@ -68,46 +69,46 @@ for (y in 2000:2020) {
       # Open the monthly file that contain SDM by location
       dat <- 
         ncdf4::nc_open(
-          paste0("G:/My Drive/Data/SDM/Historical CPS GAMs BRTs ERDDAP ROMS Domain/",
+          paste0("G:/My Drive/Data/SDM/Albacore/alb_",
                  paste0(as.character(m),
                         paste0("_",
-                               paste0(as.character(y),"_SDMs.nc")))))
+                               paste0(as.character(y),"_mboost_roms.nc")))))
       
       set_long <- ncdf4::ncvar_get(dat, "lon")
       set_lat <- ncdf4::ncvar_get(dat, "lat")
-      psdn.date.sdm <- ncdf4::ncvar_get(dat, "time")
-      psdn.sdm <- ncdf4::ncvar_get(dat, "sardGAM")
+      albc.date.sdm <- ncdf4::ncvar_get(dat, "time")
+      albc.sdm <- ncdf4::ncvar_get(dat, "predGAMBOOST")
       ncdf4::nc_close(dat)			
       
       # Reshape the 3D array so we can map it, change the time field to be date
-      dimnames(psdn.sdm) <- list(set_long = set_long, set_lat = set_lat, psdn.date.sdm = psdn.date.sdm)
-      sdmMelt <- reshape2::melt(psdn.sdm, value.name = "psdn.sdm")
-      sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$psdn.date.sdm)	
+      dimnames(albc.sdm) <- list(set_long = set_long, set_lat = set_lat, albc.date.sdm = albc.date.sdm)
+      sdmMelt <- reshape2::melt(albc.sdm, value.name = "albc.sdm")
+      sdmMelt$set_date <- as.Date("1900-01-01") + days(sdmMelt$albc.date.sdm)	
       
       # Merge to distance from port area to a set of coordinates
       distPorts <- readRDS(
-        paste0("Participation/SDMs/port_dist_psdn/portDist_",
+        paste0("Participation/SDMs/port_dist_albc/portDist_",
                paste0(as.character(j), ".rds")))
       sdmMelt <- merge(sdmMelt, distPorts, by = c('set_lat', 'set_long'), all.x = TRUE, all.y = FALSE) %>% 
         mutate(LANDING_DAY = day(set_date))
       
       # Calculate daily SDM level within port radius
       for (z in 1:max(sdmMelt$LANDING_DAY)) {
-        dat_prob_30  <- sdmMelt %>% dplyr::filter(dist <= 30)  %>% dplyr::filter(LANDING_DAY == z)
-        dat_prob_60  <- sdmMelt %>% dplyr::filter(dist <= 60)  %>% dplyr::filter(LANDING_DAY == z)
-        SDM_mean_30  <- mean(dat_prob_30$psdn.sdm, na.rm = TRUE)
-        SDM_mean_60  <- mean(dat_prob_60$psdn.sdm, na.rm = TRUE)
-        sdm.psdn <- sdm.psdn %>%
+        dat_prob_90  <- sdmMelt %>% dplyr::filter(dist <= 90)  %>% dplyr::filter(LANDING_DAY == z)
+        dat_prob_180  <- sdmMelt %>% dplyr::filter(dist <= 180)  %>% dplyr::filter(LANDING_DAY == z)
+        SDM_mean_90  <- mean(dat_prob_30$albc.sdm, na.rm = TRUE)
+        SDM_mean_180  <- mean(dat_prob_60$albc.sdm, na.rm = TRUE)
+        sdm.albc <- sdm.albc %>%
           add_row(LANDING_YEAR = y, 
                   LANDING_MONTH = m, 
                   LANDING_DAY = z, 
                   PORT_AREA_CODE = as.character(port_area_coord[j, 1]), 
-                  PSDN_SDM_30  = SDM_mean_30, 
-                  PSDN_SDM_60  = SDM_mean_60)
-        rm(dat_prob_30, dat_prob_60, SDM_mean_30, SDM_mean_60)
+                  albc_SDM_90  = SDM_mean_90, 
+                  albc_SDM_180  = SDM_mean_180)
+        rm(dat_prob_90, dat_prob_180, SDM_mean_90, SDM_mean_180)
       }
       print(paste("Year:", y, "; month:", m, "--", "Port area:",j))
-      saveRDS(sdm.psdn, file = "Participation/SDMs/sdm_psdn_update.rds")
+      saveRDS(sdm.albc, file = "Participation/SDMs/sdm_albc.rds")
     }
   }
 }
