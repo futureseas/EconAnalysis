9482ff1210f438c85dab436528e1a6e1506ff22d
Felipe Quezada <fequezad@ucsc.edu>
2024-11-11 09:00:55 -0300

    Update check_comments.R

--- Participation/R/check_comments.R
+++ Participation/R/check_comments.R
@@ -9,10 +9,11 @@ rm(list=ls())
 library(tidyverse)
 
 ## Load data ##
-c4 <- read.csv(file = "G:\\Mi unidad\\Data\\Anonymised data\\rdo_Stata_c4_full_noid.csv") 
-c5 <- read.csv(file = "G:\\Mi unidad\\Data\\Anonymised data\\rdo_Stata_c5_full_v2_noid.csv") 
-c6 <- read.csv(file = "G:\\Mi unidad\\Data\\Anonymised data\\rdo_Stata_c6_full_noid.csv") 
-c7 <- read.csv(file = "G:\\Mi unidad\\Data\\Anonymised data\\rdo_Stata_c7_full_noid.csv") 
+c4 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c4_full_noid.csv") 
+c5 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c5_full_v2_noid.csv") 
+c6 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c6_full_noid.csv") 
+c7 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c7_full_noid.csv") 
+
 
 ### Revisar autocorrelacion de SDM por dia
 
@@ -63,3 +64,33 @@ ggplot(data_c4, aes(x = set_date, y = mean_sdm, group = selection)) +
   theme_minimal() +
   theme(axis.text.x = element_blank()) + 
   facet_wrap(~ selection, scales = "free_y")
+
+
+#################################
+### Daily participation graph ###
+#################################
+
+df.subs <- c4
+
+%>%
+  dplyr::select(c(VESSEL_NUM, Species_Dominant, selection, set_month,
+                  set_date, set_year, group_all)) %>%
+  dplyr::filter(set_year == 2013,
+                set_month ==10,
+                group_all == 4) %>%
+  dplyr::filter(VESSEL_NUM == "648720" | VESSEL_NUM == "643518" |
+                  VESSEL_NUM == "598813" | VESSEL_NUM == "625449" |
+                  VESSEL_NUM == "WN5102SK") %>% drop_na() %>%   
+  group_by(VESSEL_NUM) %>%
+  dplyr::mutate(Vessel = cur_group_id()) %>%
+  ungroup() %>% rename(Month = set_date) %>%
+  rename(Species = Species_Dominant) %>%
+  mutate(Species = ifelse(Species == "CMCK", "Chub Mackerel", 
+                          ifelse(Species == "MSQD", "Market Squid",
+                                 ifelse(Species == "NANC", "Northern Anchovy",
+                                        "Pacific Sardine"))))
+
+
+ggplot(df.subs, aes(x=Month, y=Vessel, color=Species)) +
+  geom_point(size=4)
+
