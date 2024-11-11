515050f4ee289e106ac16d5c250db03b7284213c
Felipe Quezada <fequezad@ucsc.edu>
2024-11-11 14:09:56 -0300

    Update check_comments.R

--- Participation/R/check_comments.R
+++ Participation/R/check_comments.R
@@ -5,14 +5,18 @@
 gc()
 rm(list=ls())
 
+#dir = "H:\\My Drive\\"
+dir = "G:\\Mi unidad\\"
+
+
 ## Load packages ##
 library(tidyverse)
 
 ## Load data ##
-c4 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c4_full_noid.csv") 
-c5 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c5_full_v2_noid.csv") 
-c6 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c6_full_noid.csv") 
-c7 <- read.csv(file = "H:\\My Drive\\Data\\Anonymised data\\rdo_Stata_c7_full_noid.csv") 
+c4 <- read.csv(file = paste0(dir, "Data\\Anonymised data\\rdo_Stata_c4_full_noid.csv")) 
+c5 <- read.csv(file = paste0(dir, "Data\\Anonymised data\\rdo_Stata_c5_full_v2_noid.csv")) 
+c6 <- read.csv(file = paste0(dir, "Data\\Anonymised data\\rdo_Stata_c6_full_noid.csv")) 
+c7 <- read.csv(file = paste0(dir, "Data\\Anonymised data\\rdo_Stata_c7_full_noid.csv")) 
 
 
 ### Revisar autocorrelacion de SDM por dia
